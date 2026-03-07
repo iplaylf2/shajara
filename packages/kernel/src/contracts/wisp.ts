56919698de2a4991e1ca5d7585665ef40dbc5197
i-play-lf2 <914637118@qq.com>
2026-03-07 07:09:54 +0000

    refactor(wisp): reorder declarations and tighten incantation args typing

--- packages/kernel/src/contracts/wisp.ts
+++ packages/kernel/src/contracts/wisp.ts
@@ -1,22 +1,5 @@
 import type { Echo, Sigil } from "./sigil";
-
-export type Incantation<Args extends unknown[], Relic> = (...args: Args) => Wisp<Relic>;
-
-// oxlint-disable-next-line id-length
-export type Resonance<S extends Sigil, Relic> = Incantation<[echo: Echo<S>], Relic>;
-
-export type Ritual<Relic> = Incantation<[], Relic>;
-
-// oxlint-disable-next-line id-length
-export function evoke<S extends Sigil>(sigil: S): Wisp<Echo<S>> {
-  return stirringWisp(sigil, restingWisp);
-}
-
-export type Wisp<Relic> = StirringWisp<Sigil, Relic> | RestingWisp<Relic>;
-
-export function restingWisp<Relic>(relic: Relic): RestingWisp<Relic> {
-  return { bearing: "resting", relic };
-}
+import type { UnknownArray } from "type-fest";
 
 // oxlint-disable-next-line id-length
 export function stirringWisp<S extends Sigil, Relic>(
@@ -26,10 +9,20 @@ export function stirringWisp<S extends Sigil, Relic>(
   return { bearing: "stirring", resonate, sigil };
 }
 
-export interface RestingWisp<Relic> {
-  readonly bearing: "resting";
-  readonly relic: Relic;
+export function restingWisp<Relic>(relic: Relic): RestingWisp<Relic> {
+  return { bearing: "resting", relic };
 }
+
+// oxlint-disable-next-line id-length
+export function evoke<S extends Sigil>(sigil: S): Wisp<Echo<S>> {
+  return stirringWisp(sigil, restingWisp);
+}
+
+export type Ritual<Relic> = Incantation<[], Relic>;
+
+export type Incantation<Args extends UnknownArray, Relic> = (...args: Args) => Wisp<Relic>;
+
+export type Wisp<Relic> = StirringWisp<Sigil, Relic> | RestingWisp<Relic>;
 
 // oxlint-disable-next-line id-length
 export interface StirringWisp<S extends Sigil, Relic> {
@@ -37,3 +30,11 @@ export interface StirringWisp<S extends Sigil, Relic> {
   readonly sigil: S;
   readonly resonate: Resonance<S, Relic>;
 }
+
+export interface RestingWisp<Relic> {
+  readonly bearing: "resting";
+  readonly relic: Relic;
+}
+
+// oxlint-disable-next-line id-length
+export type Resonance<S extends Sigil, Relic> = Incantation<[echo: Echo<S>], Relic>;
