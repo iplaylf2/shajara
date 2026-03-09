b031ad2b13d276ddf0652f91b2375d7b3d732da6
i-play-lf2 <914637118@qq.com>
2026-03-09 07:54:35 +0000

    refactor(kernel): use futures for single-settlement race and resource results

--- packages/kernel/src/primitives/race.ts
+++ packages/kernel/src/primitives/race.ts
@@ -1,10 +1,9 @@
 import type { ArrayValues, NonEmptyTuple } from "type-fest";
-import type { Failure, MessageKey, Ritual, ScopeRef, Wisp } from "#src/contracts";
+import type { Failure, FutureResolverKey, Ritual, ScopeRef, Wisp } from "#src/contracts";
+import { awaitFuture, fork, future, halt, settleFuture, spawn } from "#src/sigils";
 import { awaitScopeConverged, park } from "#src/primitives-kit";
 import { either, readonlyArray } from "fp-ts";
-import { fork, halt, receive, self, send, spawn } from "#src/sigils";
 import type { Either } from "#src/utils";
-import { messageKey } from "#src/contracts";
 import { pipe } from "fp-ts/function";
 import { supervisorScopeSpec } from "#src/scopes";
 import { wisp } from "#src/internal/fp";
@@ -12,19 +11,17 @@ import { wisp } from "#src/internal/fp";
 export function race<BranchReturns extends NonEmptyTuple<unknown>>(
   branches: RaceBranches<BranchReturns>,
 ): Wisp<Either<Failure, ArrayValues<BranchReturns>>> {
-  const raceMessageKey = messageKey<Either<Failure, ArrayValues<BranchReturns>>>();
-
   return pipe(
     wisp.Do,
-    wisp.bindF("callerSelf", self),
-    wisp.bindF("arenaSelf", ({ callerSelf: { scopeRef: callerRef } }) =>
-      spawn(raceArena(branches, callerRef, raceMessageKey), supervisorScopeSpec()),
+    wisp.bindF("raceFuture", () => future<Either<Failure, ArrayValues<BranchReturns>>>()),
+    wisp.bindF("arenaSelf", ({ raceFuture: [, raceFutureResolverKey] }) =>
+      spawn(raceArena(branches, raceFutureResolverKey), supervisorScopeSpec()),
     ),
-    wisp.chainF(({ arenaSelf: { scopeRef: arenaRef }, callerSelf: { scopeRef: callerRef } }) =>
-      fork(arenaFailureRelay(arenaRef, callerRef, raceMessageKey)),
+    wisp.chainFirstF(
+      ({ arenaSelf: { scopeRef: arenaRef }, raceFuture: [, raceFutureResolverKey] }) =>
+        fork(arenaFailureRelay(arenaRef, raceFutureResolverKey)),
     ),
-    wisp.chainF(() => receive(raceMessageKey)),
-    wisp.map(({ value }) => value),
+    wisp.chainF(({ raceFuture: [raceFutureKey] }) => awaitFuture(raceFutureKey)),
   );
 }
 
@@ -34,14 +31,13 @@ type RaceBranches<BranchReturns extends NonEmptyTuple<unknown>> = {
 
 function raceArena(
   branches: ReadonlyArray<Ritual<unknown>>,
-  callerRef: ScopeRef<unknown>,
-  raceMessageKey: MessageKey<Either<Failure, unknown>>,
+  raceFutureResolverKey: FutureResolverKey<Either<Failure, unknown>>,
 ): Ritual<never> {
   return () =>
     pipe(
       branches,
       readonlyArray.map((branch) =>
-        pipe(fork(branchRunner(branch, callerRef, raceMessageKey)), wisp.liftF),
+        pipe(fork(branchRunner(branch, raceFutureResolverKey)), wisp.liftF),
       ),
       wisp.sequence,
       wisp.chain(() => park()),
@@ -50,26 +46,24 @@ function raceArena(
 
 function arenaFailureRelay(
   arenaRef: ScopeRef<unknown>,
-  callerRef: ScopeRef<unknown>,
-  raceMessageKey: MessageKey<Either<Failure, unknown>>,
+  raceFutureResolverKey: FutureResolverKey<Either<Failure, unknown>>,
 ): Ritual<void> {
   return () =>
     pipe(
       arenaRef,
       awaitScopeConverged,
-      wisp.chainF((value) => send(callerRef, raceMessageKey, value)),
+      wisp.chainF((value) => settleFuture(raceFutureResolverKey, value)),
     );
 }
 
 function branchRunner(
   branch: Ritual<unknown>,
-  callerRef: ScopeRef<unknown>,
-  raceMessageKey: MessageKey<Either<Failure, unknown>>,
+  raceFutureResolverKey: FutureResolverKey<Either<Failure, unknown>>,
 ): Ritual<never> {
   return () =>
     pipe(
       branch(),
-      wisp.chainF((value) => send(callerRef, raceMessageKey, either.right(value))),
+      wisp.chainF((value) => settleFuture(raceFutureResolverKey, either.right(value))),
       wisp.chainF(() => halt()),
     );
 }
--- packages/kernel/src/primitives/resource.ts
+++ packages/kernel/src/primitives/resource.ts
@@ -1,10 +1,8 @@
-import type { Failure, MessageKey, Ritual, ScopeRef, Wisp } from "#src/contracts";
+import type { Failure, FutureResolverKey, Ritual, ScopeRef, Wisp } from "#src/contracts";
+import { awaitFuture, fork, future, settleFuture, spawn } from "#src/sigils";
 import { awaitScopeConverged, park } from "#src/primitives-kit";
-import { fork, receive, self, send, spawn } from "#src/sigils";
 import type { Either } from "#src/utils";
 import { either } from "fp-ts";
-import { messageKey } from "#src/contracts/message-key";
-import { narrowAs } from "#src/utils";
 import { pipe } from "fp-ts/function";
 import { supervisorScopeSpec } from "#src/scopes";
 import { wisp } from "#src/internal/fp";
@@ -12,23 +10,17 @@ import { wisp } from "#src/internal/fp";
 export function resource<ProvidedValue>(
   body: ResourceBody<ProvidedValue>,
 ): Wisp<Either<Failure, ProvidedValue>> {
-  const resourceMessageKey = messageKey<Either<Failure, ProvidedValue>>();
-
   return pipe(
     wisp.Do,
-    wisp.bindF("callerSelf", self),
-    wisp.bindF("resourceSupervisorSelf", ({ callerSelf: { scopeRef: callerRef } }) =>
-      spawn(resourceSupervisor(body, callerRef, resourceMessageKey), supervisorScopeSpec()),
+    wisp.bindF("resourceFuture", () => future<Either<Failure, ProvidedValue>>()),
+    wisp.bindF("resourceSupervisorSelf", ({ resourceFuture: [, resourceFutureResolverKey] }) =>
+      spawn(resourceSupervisor(body, resourceFutureResolverKey), supervisorScopeSpec()),
     ),
-    wisp.chainF(
-      ({
-        callerSelf: { scopeRef: callerRef },
-        resourceSupervisorSelf: { scopeRef: supervisorRef },
-      }) => fork(resourceFailureRelay(supervisorRef, callerRef, resourceMessageKey)),
+    wisp.chainFirstF(
+      ({ resourceFuture: [, resourceFutureResolverKey], resourceSupervisorSelf: { scopeRef } }) =>
+        fork(resourceFailureRelay(scopeRef, resourceFutureResolverKey)),
     ),
-    wisp.chainF(() => receive(resourceMessageKey)),
-    wisp.map(({ value }) => value),
-    wisp.map(narrowAs<Either<Failure, ProvidedValue>>()),
+    wisp.chainF(({ resourceFuture: [resourceFutureKey] }) => awaitFuture(resourceFutureKey)),
   );
 }
 
@@ -40,14 +32,13 @@ export type ResourceProvide<ProvidedValue> = (value: ProvidedValue) => Wisp<neve
 
 function resourceSupervisor<ProvidedValue>(
   body: ResourceBody<ProvidedValue>,
-  callerRef: ScopeRef<unknown>,
-  resourceMessageKey: MessageKey<Either<Failure, ProvidedValue>>,
+  resourceFutureResolverKey: FutureResolverKey<Either<Failure, ProvidedValue>>,
 ): Ritual<never> {
   return () =>
     pipe(
       body((value) =>
         pipe(
-          send(callerRef, resourceMessageKey, either.right(value)),
+          settleFuture(resourceFutureResolverKey, either.right(value)),
           wisp.liftF,
           wisp.chain(() => park()),
         ),
@@ -58,13 +49,12 @@ function resourceSupervisor<ProvidedValue>(
 
 function resourceFailureRelay<ProvidedValue>(
   supervisorRef: ScopeRef<unknown>,
-  callerRef: ScopeRef<unknown>,
-  resourceMessageKey: MessageKey<Either<Failure, ProvidedValue>>,
+  resourceFutureResolverKey: FutureResolverKey<Either<Failure, ProvidedValue>>,
 ): Ritual<void> {
   return () =>
     pipe(
       supervisorRef,
       awaitScopeConverged,
-      wisp.chainF((value) => send(callerRef, resourceMessageKey, value)),
+      wisp.chainF((value) => settleFuture(resourceFutureResolverKey, value)),
     );
 }
