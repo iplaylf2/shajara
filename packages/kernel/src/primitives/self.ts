a3cf88ea99a95669f190e55890693ff0fd395fdd
i-play-lf2 <914637118@qq.com>
2026-03-10 04:52:34 +0000

    refactor(kernel): simplify self primitive typing

--- packages/kernel/src/primitives/self.ts
+++ packages/kernel/src/primitives/self.ts
@@ -1,12 +1,10 @@
 import type { ScopeRef, Wisp } from "#src/contracts";
 import type { SelfDescriptor } from "#src/sigils";
-import { narrowAs } from "#src/utils";
-import { pipe } from "fp-ts/function";
 import { self as selfSigil } from "#src/sigils";
 import { wisp } from "#src/internal/fp";
 
 export type { SelfDescriptor } from "#src/sigils";
 
 export function self<Scope extends ScopeRef<unknown>>(): Wisp<SelfDescriptor<Scope>> {
-  return pipe(selfSigil(), wisp.liftF, wisp.map(narrowAs<any>()));
+  return wisp.liftF(selfSigil()) as Wisp<SelfDescriptor<Scope>>;
 }
